2596f7037ae02f7fb8cef2b8f526b899319c4ff4
Felipe Gomes <felipeglacerdaa@gmail.com>
2026-04-09 12:01:19 -0300

    update page

--- src/app/convite/[code]/page.tsx
+++ src/app/convite/[code]/page.tsx
@@ -2,8 +2,8 @@ import { prisma } from '@/lib/prisma';
 import { notFound } from 'next/navigation';
 import PublicInvitationContent from './PublicInvitationContent';
 
-export default async function PublicInvitationPage({ params }: { params: { code: string } }) {
-  const code = params.code;
+export default async function PublicInvitationPage({ params }: { params: Promise<{ code: string }> }) {
+  const { code } = await params;
 
   try {
     const invitation = await prisma.invitation.findUnique({
